adfb9e3e3ede316da0db535c1fa8f7189b448737
mcdylanb <dylansatgalab@gmail.com>
2019-10-25 16:20:08 +0800

    Renamed variables
    +for easier identification
      ex:
        from: user_google
        to: google_user

--- TMS_GD_sync/webAutomation.py
+++ TMS_GD_sync/webAutomation.py
@@ -15,18 +15,18 @@ browser = webdriver.Chrome("C:\Dylan'sFiles\Dev\Chromium-driver\chromedriver.exe
 browser.get('http://jeepme/webapp/tms/')
 
 #gets input boxes and buttons
-username_tms = browser.find_element_by_id("ctl00_ContentPlaceHolder1_logApplication_UserName")
-password_tms = browser.find_element_by_id("ctl00_ContentPlaceHolder1_logApplication_Password")
-submit_button_tms = browser.find_element_by_id("ctl00_ContentPlaceHolder1_logApplication_LoginButton")
+tms_username_input = browser.find_element_by_id("ctl00_ContentPlaceHolder1_logApplication_UserName")
+tms_password_input = browser.find_element_by_id("ctl00_ContentPlaceHolder1_logApplication_Password")
+tms_submit_button = browser.find_element_by_id("ctl00_ContentPlaceHolder1_logApplication_LoginButton")
 
 #automatically logins using the input from above
-username_tms.send_keys('BALAGTASM')
+tms_username_input.send_keys('BALAGTASM')
 print("username pass")
 time.sleep(2)
-password_tms.send_keys('xUbwcSc+iO')
+tms_password_input.send_keys('xUbwcSc+iO')
 print("password pass")
 time.sleep(2)
-submit_button_tms.click()
+tms_submit_button.click()
 
 #opens page with data
 browser.get("http://jeepme/webapp/tms/WebApp/TimeAttendanceLogs.aspx")
@@ -41,22 +41,22 @@ export.click()
 browser.get('https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dgoogle%2Blogin%26rlz%3D1C1GCEU_enPH868PH868%26oq%3Dgoogle%2Blogin%26aqs%3Dchrome..69i57j69i64.2367j0j1%26sourceid%3Dchrome%26ie%3DUTF-8&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
 
 #gets input boxes and buttons for username
-username_google = browser.find_element_by_id("identifierId")
-next_btn_username_google = browser.find_element_by_id("identifierNext")
+google_username_inp = browser.find_element_by_id("identifierId")
+googlenext_btn_username = browser.find_element_by_id("identifierNext")
 
 
-username_google.send_keys(google_username)
+google_username_inp.send_keys(google_username)
 time.sleep(2)
-next_btn_username_google.click()
+googlenext_btn_username.click()
 time.sleep(2)
 
 #gets input boxes and buttons for password
-password_google = browser.find_element_by_name("password")
-next_btn_password_google = browser.find_element_by_id('passwordNext')
+google_password_input = browser.find_element_by_name("password")
+google_next_btn_password = browser.find_element_by_id('passwordNext')
 
-password_google.send_keys(google_password)
+google_password_input.send_keys(google_password)
 time.sleep(2)
-next_btn_password_google.click()
+google_next_btn_password.click()
 time.sleep(2)
 
 browser.get('https://drive.google.com/drive/u/0/folders/1by0DliV7j4oHm_-RT0XA6IQ3SCRmkmPf')
